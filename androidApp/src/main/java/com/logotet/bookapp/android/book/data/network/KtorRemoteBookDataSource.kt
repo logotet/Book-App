b7a221e983f9be27765949ac29425b32c1e7df6c
v.vasilev <v.vasilev@up2technology.com>
2024-11-26 08:32:57 +0200

    refactor: Apply flow wrapper to data result

--- androidApp/src/main/java/com/logotet/bookapp/android/book/data/network/KtorRemoteBookDataSource.kt
+++ androidApp/src/main/java/com/logotet/bookapp/android/book/data/network/KtorRemoteBookDataSource.kt
@@ -1,12 +1,15 @@
 package com.logotet.bookapp.android.book.data.network
 
-import com.logotet.bookapp.android.book.data.dto.BookDetailsDto
-import com.logotet.bookapp.android.book.data.dto.BookItemsDto
+import com.logotet.bookapp.android.book.data.network.dto.BookDetailsDto
+import com.logotet.bookapp.android.book.data.network.dto.BookItemsDto
 import com.logotet.bookapp.android.core.domain.result.DataError
 import com.logotet.bookapp.android.core.domain.result.DataResult
 import io.ktor.client.HttpClient
 import io.ktor.client.request.get
 import io.ktor.client.request.parameter
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flowOn
 
 class KtorRemoteBookDataSource(
     private val httpClient: HttpClient
@@ -14,8 +17,8 @@ class KtorRemoteBookDataSource(
     override suspend fun searchBooks(
         query: String,
         resultLimit: Int?
-    ): DataResult<BookItemsDto, DataError.Remote> =
-        makeRequest {
+    ): Flow<DataResult<BookItemsDto, DataError.Remote>> =
+        makeRequest<BookItemsDto> {
             httpClient.get(
                 urlString = "$BASE_URL/search.json"
             ) {
@@ -27,17 +30,16 @@ class KtorRemoteBookDataSource(
                     "key,title,author_name,author_key,cover_edition_key,cover_i,ratings_average,ratings_count,first_publish_year,language,number_of_pages_median,edition_count"
                 )
             }
-        }
+        }.flowOn(Dispatchers.IO)
 
     override suspend fun getBookDetails(
         bookWorkId: String
-    ): DataResult<BookDetailsDto, DataError.Remote> =
+    ): Flow<DataResult<BookDetailsDto, DataError.Remote>> =
         makeRequest<BookDetailsDto> {
             httpClient.get(
                 urlString = "$BASE_URL/works/$bookWorkId.json"
             )
-        }
-
+        }.flowOn(Dispatchers.IO)
 
     companion object {
         private const val BASE_URL = "https://openlibrary.org"
