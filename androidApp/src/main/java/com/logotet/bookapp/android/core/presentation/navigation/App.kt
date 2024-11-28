2441fe8fc7792313f36d15684441cba139eed2db
v.vasilev <v.vasilev@up2technology.com>
2024-11-28 09:27:13 +0200

    feat: Navigate from book list item to details screen

--- androidApp/src/main/java/com/logotet/bookapp/android/core/presentation/navigation/App.kt
+++ androidApp/src/main/java/com/logotet/bookapp/android/core/presentation/navigation/App.kt
@@ -3,12 +3,16 @@ package com.logotet.bookapp.android.core.presentation.navigation
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.logotet.bookapp.android.book.presentation.details.BookListScreen
+import com.logotet.bookapp.android.book.presentation.list.BookListViewModel
 import com.logotet.bookapp.android.core.presentation.theme.AppTheme
+import org.koin.androidx.compose.koinViewModel
 
 @Composable
 fun App() {
@@ -24,11 +28,20 @@ fun App() {
                 startDestination = Route.BookList
             ) {
                 composable<Route.BookList> {
-
+                    BookListScreen(
+                        viewModel = koinViewModel<BookListViewModel>(),
+                        navigateToBookDetails = { bookId ->
+                            navController.navigate(
+                                Route.BookDetails(
+                                    bookId
+                                )
+                            )
+                        }
+                    )
                 }
 
-                composable<Route.BookDetails> {
-
+                composable<Route.BookDetails> { backStackEntry ->
+                    Text(backStackEntry.id)
                 }
             }
         }
