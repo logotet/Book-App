8b3942150280d50ea433c63b872fe6005fda53a2
v.vasilev <v.vasilev@up2technology.com>
2024-11-28 11:38:17 +0200

    feat: Set app colors file

---
+++ androidApp/src/main/java/com/logotet/bookapp/android/core/presentation/theme/Colors.kt
@@ -0,0 +1,10 @@
+package com.logotet.bookapp.android.core.presentation.theme
+
+import androidx.compose.ui.graphics.Color
+
+val DarkBlue = Color(0xFF0B405E)
+val DesertWhite = Color(0xFFF7F7F7)
+val SandYellow = Color(0xFFFFBD64)
+val LightBlue = Color(0xFF9AD9FF)
+val CadiumPurple = Color(0xFFbe1622)
+val Gevser = Color(0xFFD4DCE4)
