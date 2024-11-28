12766ccd1846592197295397e80daea626f1b18c
v.vasilev <v.vasilev@up2technology.com>
2024-11-28 11:38:28 +0200

    feat: CoilImagePainter composable

---
+++ androidApp/src/main/java/com/logotet/bookapp/android/core/presentation/composable/CoilImagePainter.kt
@@ -0,0 +1,50 @@
+package com.logotet.bookapp.android.core.presentation.composable
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.res.painterResource
+import coil3.compose.rememberAsyncImagePainter
+import com.logotet.bookapp.android.R
+
+@Composable
+fun rememberCoilImagePainter(
+    imageUrl: String,
+    placeholder: Int = R.drawable.image_book_cover_placeholder
+): Painter {
+    var imageLoadResult by remember {
+        mutableStateOf<Result<Painter>?>(null)
+    }
+
+    val bookCoverPainter = rememberAsyncImagePainter(
+        model = imageUrl,
+        onSuccess = {
+            imageLoadResult =
+                if (it.painter.intrinsicSize.width > 1 && it.painter.intrinsicSize.height > 1) {
+                    Result.success(it.painter)
+                } else {
+                    Result.failure(Exception())
+                }
+        },
+        onError = {
+            it.result.throwable.printStackTrace()
+            imageLoadResult = Result.failure(it.result.throwable)
+        }
+    )
+
+    val painter =
+        when (val result = imageLoadResult) {
+            null -> painterResource(placeholder)
+
+            else -> if (result.isSuccess) {
+                bookCoverPainter
+            } else {
+                painterResource(placeholder)
+            }
+        }
+
+    return painter
+}
