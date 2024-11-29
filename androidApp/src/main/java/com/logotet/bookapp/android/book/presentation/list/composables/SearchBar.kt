74089ab726b8e8fdeaf420d4627c83e9fb480da5
v.vasilev <v.vasilev@up2technology.com>
2024-11-29 16:06:19 +0200

    refactor: Custom search bar

--- androidApp/src/main/java/com/logotet/bookapp/android/book/presentation/list/composables/SearchBar.kt
+++ androidApp/src/main/java/com/logotet/bookapp/android/book/presentation/list/composables/SearchBar.kt
@@ -2,24 +2,32 @@
 
 package com.logotet.bookapp.android.book.presentation.list.composables
 
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Clear
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
-import androidx.compose.material3.SearchBar
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.logotet.bookapp.android.core.presentation.theme.AppTheme
+import com.logotet.bookapp.android.core.presentation.theme.Dimensions
 
 private const val EMPTY_STRING = ""
 private val searchBarHeight = 50.dp
@@ -31,34 +39,47 @@ fun SearchBar(
 ) {
     var query by remember { mutableStateOf(EMPTY_STRING) }
 
-    SearchBar(
-        leadingIcon = { Icon(imageVector = Icons.Default.Search, contentDescription = null) },
-        trailingIcon = {
-            IconButton(
-                onClick = { query = EMPTY_STRING },
-                content = {
-                    Icon(imageVector = Icons.Default.Clear, contentDescription = null)
-                }
-            )
-        },
-        query = query,
-        onQueryChange = { newValue ->
-            query = newValue
-            search(newValue)
-        },
-        onSearch = {},
-        active = true,
-        onActiveChange = {},
+    Row(
         modifier = modifier
-            .fillMaxWidth()
+            .padding(
+                horizontal = Dimensions.Spacing.medium,
+                vertical = Dimensions.Spacing.xlarge
+            )
             .height(searchBarHeight)
-    ) { }
+            .fillMaxWidth()
+            .clip(MaterialTheme.shapes.medium)
+    ) {
+        TextField(
+            modifier = Modifier.fillMaxSize(),
+            leadingIcon = {
+                Icon(imageVector = Icons.Default.Search, contentDescription = null)
+            },
+            trailingIcon = {
+                IconButton(
+                    onClick = { query = EMPTY_STRING },
+                    content = {
+                        Icon(imageVector = Icons.Default.Clear, contentDescription = null)
+                    }
+                )
+            },
+            value = query,
+            onValueChange = { newValue ->
+                query = newValue
+                search(query)
+            },
+            singleLine = true,
+            colors = TextFieldDefaults.colors(
+                focusedIndicatorColor = Color.Transparent,
+                unfocusedIndicatorColor = Color.Transparent,
+            )
+        )
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun SearchBarPreview() {
     AppTheme {
-       SearchBar(search = {})
+        SearchBar(search = {})
     }
 }
