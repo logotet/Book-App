cbd23aee16fce750689dee01dd6d0d223540ed40
v.vasilev <v.vasilev@up2technology.com>
2024-12-02 18:01:11 +0200

    refactor: Move result handler to BaseViewModel

--- androidApp/src/main/java/com/logotet/bookapp/android/core/presentation/BaseViewModel.kt
+++ androidApp/src/main/java/com/logotet/bookapp/android/core/presentation/BaseViewModel.kt
@@ -2,11 +2,13 @@ package com.logotet.bookapp.android.core.presentation
 
 import androidx.lifecycle.ViewModel
 import com.logotet.bookapp.android.core.domain.result.AppError
+import com.logotet.bookapp.android.core.domain.result.DataError
+import com.logotet.bookapp.android.core.domain.result.DataResult
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 
-abstract class BaseViewModel<T> : ViewModel(){
+abstract class BaseViewModel<T> : ViewModel() {
     protected val _state: MutableStateFlow<ScreenState<T>> =
         MutableStateFlow(ScreenState.Loading)
     val state: StateFlow<ScreenState<T>> = _state.asStateFlow()
@@ -20,5 +22,26 @@ abstract class BaseViewModel<T> : ViewModel(){
 
     sealed interface Event {
         data class ShowError(val error: AppError) : Event
+    }
+
+    fun DataResult<T, DataError>.handleResult(
+        onSuccess: (T) -> Unit = {},
+        onError: (AppError) -> Unit = {},
+    ) {
+        when (val result = this) {
+            is DataResult.Loading -> {
+                _state.value = ScreenState.Loading
+            }
+
+            is DataResult.Success -> {
+                _state.value = ScreenState.Success(result.data)
+                onSuccess(result.data)
+            }
+
+            is DataResult.Error -> {
+                _state.value = ScreenState.Error(result.error)
+                onError(result.error)
+            }
+        }
     }
 }
