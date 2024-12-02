21302859645d7426dc3a28c7f3225eb1056513a5
v.vasilev <v.vasilev@up2technology.com>
2024-12-02 17:58:53 +0200

    refactor: Rename data result mapper

--- androidApp/src/main/java/com/logotet/bookapp/android/core/domain/result/ResultMapper.kt
+++ androidApp/src/main/java/com/logotet/bookapp/android/core/domain/result/ResultMapper.kt
@@ -3,14 +3,14 @@ package com.logotet.bookapp.android.core.domain.result
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
 
-fun <Remote, Local> Flow<DataResult<Remote, DataError.Remote>>.mapSuccess(
-    converter: suspend (Remote) -> Local
-): Flow<DataResult<Local, DataError.Remote>> =
+fun <Fetched, Local> Flow<DataResult<Fetched, DataError>>.mapSuccess(
+    convert: suspend (Fetched) -> Local
+): Flow<DataResult<Local, DataError>> =
     map { result ->
         when (result) {
             is DataResult.Loading -> result
-            is DataResult.Success<Remote> -> {
-                DataResult.Success(converter(result.data))
+            is DataResult.Success<Fetched> -> {
+                DataResult.Success(convert(result.data))
             }
             is DataResult.Error -> {
                 result
