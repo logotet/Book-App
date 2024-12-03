f935885e2b8ec76de250615fe92f836e0acadf7e
v.vasilev <v.vasilev@up2technology.com>
2024-12-03 18:51:37 +0200

    feat: Implement save-book to favorites UI flow

--- androidApp/src/main/java/com/logotet/bookapp/android/book/presentation/details/BookDetailsScreen.kt
+++ androidApp/src/main/java/com/logotet/bookapp/android/book/presentation/details/BookDetailsScreen.kt
@@ -1,5 +1,6 @@
 package com.logotet.bookapp.android.book.presentation.details
 
+import android.util.Log
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -16,9 +17,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
@@ -44,6 +42,7 @@ fun BookDetailsScreen(
     navigateBack: () -> Unit
 ) {
     val bookDetailsState by viewModel.state.collectAsState()
+    val isSaved by viewModel.isSaved.collectAsState()
 
     LaunchedEffect(Unit) {
         viewModel.getBookDetails(bookId)
@@ -56,12 +55,20 @@ fun BookDetailsScreen(
             )
         }
     ) { bookWithDetails ->
-        var isSaved by remember { mutableStateOf(false) }
+        val book = bookWithDetails.first
+
         BookDetailsContent(
             bookWithDetails = bookWithDetails,
             isSaved = isSaved,
-            setBookFavoriteStatus = {
-                isSaved = it
+            setBookFavoriteStatus = { isSaved ->
+                if (isSaved)
+                    viewModel.onAction(
+                        BookDetailsViewModel.BookDetailsAction.DeleteBook(book)
+                    )
+                else
+                    viewModel.onAction(
+                        BookDetailsViewModel.BookDetailsAction.SaveBook(book)
+                    )
             }
         )
     }
--- androidApp/src/main/java/com/logotet/bookapp/android/book/presentation/details/BookDetailsViewModel.kt
+++ androidApp/src/main/java/com/logotet/bookapp/android/book/presentation/details/BookDetailsViewModel.kt
@@ -1,11 +1,14 @@
 package com.logotet.bookapp.android.book.presentation.details
 
 import androidx.lifecycle.viewModelScope
-import com.logotet.bookapp.android.book.data.DefaultBookRepository
 import com.logotet.bookapp.android.book.domain.BookRepository
-import com.logotet.bookapp.android.book.domain.model.BookDetails
+import com.logotet.bookapp.android.book.domain.model.Book
 import com.logotet.bookapp.android.book.domain.model.BookWithDetails
+import com.logotet.bookapp.android.core.domain.result.DataResult
+import com.logotet.bookapp.android.core.domain.result.onSuccess
 import com.logotet.bookapp.android.core.presentation.BaseViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.launch
 
@@ -13,11 +16,61 @@ class BookDetailsViewModel(
     private val bookRepository: BookRepository
 ) : BaseViewModel<BookWithDetails>() {
 
+    sealed interface BookDetailsAction {
+        data class SaveBook(val book: Book) : BookDetailsAction
+        data class DeleteBook(val book: Book) : BookDetailsAction
+    }
+
+    private val _isSaved: MutableStateFlow<Boolean> = MutableStateFlow(false)
+    val isSaved = _isSaved.asStateFlow()
+
     fun getBookDetails(bookId: String) {
         viewModelScope.launch {
             bookRepository.getBookDetails(bookId)
                 .collectLatest { result ->
-                   result.handleResult()
+                    result.handleResult()
+                }
+
+            bookRepository.isFavoriteBook(bookId).collectLatest { isFavoriteResult ->
+                if (isFavoriteResult is DataResult.Success) {
+                    _isSaved.value = isFavoriteResult.data
+                } else {
+                    _isSaved.value = false
+                }
+            }
+        }
+    }
+
+    fun onAction(action: BookDetailsAction) {
+        when (action) {
+            is BookDetailsAction.SaveBook -> {
+                onSaveBookAction(action)
+            }
+
+            is BookDetailsAction.DeleteBook -> {
+                onDeleteBookAction(action)
+            }
+        }
+    }
+
+    private fun onDeleteBookAction(action: BookDetailsAction.DeleteBook) {
+        val book = action.book
+
+        viewModelScope.launch {
+            bookRepository.removeBookFromFavorites(bookId = book.id)
+                .onSuccess {
+                    _isSaved.value = false
+                }
+        }
+    }
+
+    private fun onSaveBookAction(action: BookDetailsAction.SaveBook) {
+        viewModelScope.launch {
+            val book = action.book
+
+            bookRepository.insertFavoriteBook(book = book)
+                .onSuccess {
+                    _isSaved.value = true
                 }
         }
     }
