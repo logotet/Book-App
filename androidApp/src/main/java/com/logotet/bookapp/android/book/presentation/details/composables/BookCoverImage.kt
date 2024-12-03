bbfcf68815245dbc667cce48b965a600c19fd8c8
v.vasilev <v.vasilev@up2technology.com>
2024-12-03 14:53:39 +0200

    feat: Book cover with heart image composable

---
+++ androidApp/src/main/java/com/logotet/bookapp/android/book/presentation/details/composables/BookCoverImage.kt
@@ -0,0 +1,77 @@
+package com.logotet.bookapp.android.book.presentation.details.composables
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.logotet.bookapp.android.R
+import com.logotet.bookapp.android.core.presentation.theme.AppTheme
+import com.logotet.bookapp.android.core.presentation.theme.Dimensions
+
+private val imageHeight = 220.dp
+private val imageWidth = 150.dp
+private val heartSize = 30.dp
+
+@Composable
+fun BookCoverImage(
+    painter: Painter,
+    isSaved: Boolean,
+    modifier: Modifier = Modifier,
+    onHeartClick: (Boolean) -> Unit
+) {
+    Box(
+        modifier = modifier
+            .size(height = imageHeight, width = imageWidth)
+    ) {
+        Image(
+            modifier = Modifier.fillMaxSize(),
+            painter = painter,
+            contentScale = ContentScale.Crop,
+            contentDescription = null,
+        )
+
+        IconButton(
+            modifier = Modifier
+                .padding(Dimensions.Spacing.small)
+                .align(Alignment.TopEnd),
+            onClick = { onHeartClick(!isSaved) },
+        ) {
+            Icon(
+                modifier = Modifier.size(heartSize),
+                imageVector = Icons.Default.Favorite,
+                contentDescription = null,
+                tint =
+                if (isSaved)
+                    MaterialTheme.colorScheme.error
+                else
+                    MaterialTheme.colorScheme.primaryContainer
+            )
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun BookCoverImagePreview() {
+    AppTheme {
+        BookCoverImage(
+            painter = painterResource(R.drawable.image_book_cover_placeholder),
+            isSaved = false,
+            onHeartClick = {}
+        )
+    }
+}
