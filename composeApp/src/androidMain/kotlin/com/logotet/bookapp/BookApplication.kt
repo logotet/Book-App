cc89864a55b8f119a2667f046378b5bab37140e6
v.vasilev <v.vasilev@up2technology.com>
2025-02-26 11:46:21 +0200

    refactor: Koin setup for all platforms

--- composeApp/src/androidMain/kotlin/com/logotet/bookapp/BookApplication.kt
+++ composeApp/src/androidMain/kotlin/com/logotet/bookapp/BookApplication.kt
@@ -1,8 +1,9 @@
 package com.logotet.bookapp
 
 import android.app.Application
-import com.logotet.bookapp.di.appModule
+import com.logotet.bookapp.di.sharedModule
 import com.logotet.bookapp.di.databaseModule
+import com.logotet.bookapp.di.initKoin
 import com.logotet.bookapp.di.networkModule
 import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.GlobalContext.startKoin
@@ -10,9 +11,8 @@ import org.koin.core.context.GlobalContext.startKoin
 class BookApplication : Application() {
     override fun onCreate() {
         super.onCreate()
-        startKoin {
+        initKoin {
             androidContext(this@BookApplication)
-            modules(appModule, networkModule, databaseModule)
         }
     }
 }
--- composeApp/src/desktopMain/kotlin/com/logotet/bookapp/main.kt
+++ composeApp/src/desktopMain/kotlin/com/logotet/bookapp/main.kt
@@ -2,8 +2,11 @@ package com.logotet.bookapp
 
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
+import com.logotet.bookapp.di.initKoin
 
 fun main() = application {
+    initKoin()
+
     Window(
         onCloseRequest = ::exitApplication,
         title = "BookApp",
